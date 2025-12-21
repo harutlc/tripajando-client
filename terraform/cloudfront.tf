38a53043dbc176c3fb779f047f9c1107460fc4b8
Harutyun Hayrapetyan <harutyunhayrapetyan@gmail.com>
2025-12-21 14:48:06 +0400

    update ordered_cache_behavior for cloudfront

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -16,6 +16,27 @@ resource "aws_cloudfront_distribution" "website" {
     }
   }
 
+  # Don't cache index.html to ensure users get latest version after deployments
+  ordered_cache_behavior {
+    path_pattern     = "/index.html"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = true
+  }
+
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
